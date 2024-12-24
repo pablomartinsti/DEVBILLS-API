027e0113efd83a1f7aceae0b033781f2553e9734
PABLO MARTINS git config --global user.email <seuemail@exemplo.com>
2024-12-23 23:25:52 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import 'dotenv/config'
-import express, { json } from 'express'
-import cors from 'cors'
-import { setupMongo } from './database'
-import { routes } from './routes'
-import { errorHandler } from './middleware/error-handler.middleware.ts'
+import 'dotenv/config';
+import express, { json } from 'express';
+import cors from 'cors';
+import { setupMongo } from './database';
+import { routes } from './routes';
+import { errorHandler } from './middleware/error-handler.middleware.ts';
 
 setupMongo().then(() => {
+  const app = express();
+  app.use(cors({ origin: process.env.FRONT_URL }));
+  app.use(json());
+  app.use(routes);
+  app.use(errorHandler);
 
-    const app = express()
-    app.use(cors({ origin: process.env.FRONT_URL }))
-    app.use(json())
-    app.use(routes)
-    app.use(errorHandler)
-
-    app.listen(3333, () => console.log('🚀 App is running at port 3333!'))
-})
+  const PORT = process.env.PORT || 3333;
+  app.listen(PORT, () => console.log(`🚀 App is running at port ${PORT}`));
+});
