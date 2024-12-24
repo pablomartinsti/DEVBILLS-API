c5f84167a46730131c54c67aee193385c230ceef
PABLO MARTINS git config --global user.email <seuemail@exemplo.com>
2024-12-23 22:09:42 -0300

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,11 @@
-# Use uma imagem base leve do Node.js
 FROM node:18-alpine
 
-# Define o diretório de trabalho no container
 WORKDIR /home/app
 
-# Copia apenas os arquivos necessários
-COPY package*.json ./
+COPY . ./
 
-# Instala as dependências apenas para produção
-RUN npm ci --only=production
+RUN npm i
 
-# Copia o restante dos arquivos do projeto
-COPY . .
-
-# Expõe a porta 3333
 EXPOSE 3333
 
-# Define o comando para rodar a aplicação em produção
-CMD ["npm", "run", "start"]
+CMD ["npm", "run", "dev"]
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,41 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { ParamsType, validator } from '../middleware/validator.middleware';
-import { TransactionsController } from '../controllers/transactions.controller';
-import { TransactionsFactory } from '../factories/transactions.factory';
-import {
-  createTransactionSchema,
-  getDashboardSchema,
-  getFinancialEvolutionSchema,
-  indexTransactionsSchema,
-} from '../dtos/transactions.dto';
+import { ParamsType, validator } from "../middleware/validator.middleware";
+import { TransactionsController } from "../controllers/transactions.controller";
+import { TransactionsFactory } from "../factories/transactions.factory";
+import { createTransactionSchema, getDashboardSchema, getFinancialEvolutionSchema, indexTransactionsSchema } from "../dtos/transactions.dto";
 
-export const transactionsRoutes = Router();
+export const transactionsRoutes = Router()
 
-const controller = new TransactionsController(
-  TransactionsFactory.getServiceInstance(),
-);
+const controller = new TransactionsController(TransactionsFactory.getServiceInstance())
 
-transactionsRoutes.post(
-  '/',
-  validator({ schema: createTransactionSchema, type: ParamsType.BODY }),
-  controller.create,
-);
 
-transactionsRoutes.get(
-  '/',
-  validator({ schema: indexTransactionsSchema, type: ParamsType.QUERY }),
-  controller.index,
-);
+transactionsRoutes.post('/', validator({ schema: createTransactionSchema, type: ParamsType.BODY }), controller.create)
 
-transactionsRoutes.get(
-  '/dashboard',
-  validator({ schema: getDashboardSchema, type: ParamsType.QUERY }),
-  controller.getDashboard,
-);
+transactionsRoutes.get('/', validator({ schema: indexTransactionsSchema, type: ParamsType.QUERY }), controller.index)
 
-transactionsRoutes.get(
-  '/financial-evolution',
-  validator({ schema: getFinancialEvolutionSchema, type: ParamsType.QUERY }),
-  controller.getFinancialEvolution,
-);
+transactionsRoutes.get('/dashboard', validator({ schema: getDashboardSchema, type: ParamsType.QUERY }), controller.getDashboard)
+
+transactionsRoutes.get('/financial-evolution', validator({ schema: getFinancialEvolutionSchema, type: ParamsType.QUERY }), controller.getFinancialEvolution)
